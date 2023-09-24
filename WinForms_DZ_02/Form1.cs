5fae3c192615971c50ef2ce99f8e52cc84dbc115
AseevArtjom <Artjom4ik2008@gmail.com>
2023-09-24 15:29:50 +0300

    DZ_02 was done.

--- WinForms_DZ_02/Form1.cs
+++ WinForms_DZ_02/Form1.cs
@@ -6,5 +6,38 @@ namespace WinForms_DZ_02
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i.ToString() != textBox1.Text)
+            {
+                if (i > 2000)
+                {
+                    return;
+                }
+                else { i++; }
+            }
+            MessageBox.Show(i.ToString());
+            var resultDialog = MessageBox.Show("Play again?","Notification",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (resultDialog == DialogResult.Yes)
+            {
+                return;
+            }
+            else if(resultDialog == DialogResult.No)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+                          
+        }
     }
 }
